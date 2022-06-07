caa1ca40da3c5230f5bfb32ff27125f8ecefb6bd
yuvasai2002 <107022813+yuvasai2002@users.noreply.github.com>
2022-06-07 12:43:23 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class addition {
+     public static void main(String[] args)
+     {
+    	 Scanner s = new Scanner(System.in);
+    	 int a = s.nextInt();
+    	 int b = s.nextInt();
+    	 System.out.println("The addition is : "+(a+b));
+     }
+}
